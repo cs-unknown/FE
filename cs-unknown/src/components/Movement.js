7bde41dca4b5f1ccd3716477e4737dcd8f2e41cc
antilou86 <antilou86@gmail.com>
2020-01-07 18:02:18 -0500

    created an initial draft of the textbox component. it will need data from a higher component so next up i will create the buttons for the movement commands.

---
+++ cs-unknown/src/components/Movement.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Movement = () => {
+
+
+
+    return(
+        <div className="move_button_container">
+            <div>West</div>
+            <div>North</div>
+            <div>East</div>
+            <div>South</div>
+        </div>
+    )
+}
+
+export default Movement
---
+++ cs-unknown/src/components/Sidebar/Textbox.js
@@ -0,0 +1,25 @@
+import React, {useState, useEffect} from 'react';
+
+
+//for each response to a move call, the response.data will have to be passed in here as props.
+
+const Textbox = (props) => {
+    const [text, setText] = useState(["Welcome to Game Uknown.", "*A commanding voice echoes through the chamber you are standing in and rebounds throughout your skull.*","*a sense of dark dread fills your being, like when you have to use the restroom, but you are nowhere near one.*", "'Good luck figuring out how to win.'", "'MUAAAAHAHAHAHAAAAAAA'",])
+    
+
+    useEffect(()=> {
+        setText([...text] + props.newText)
+    },[props.text]) //double check this dependency array for naming convention.
+    
+    return (
+    //css will have to allow for scrolling overflow of text in this box.
+      <div className='textbox'>
+        {text.map((line, i) => {
+            <p key={"line"+i}>{line}</p>
+        })}
+      </div>
+  )
+}
+
+//this will need to be imported into Sidebar.
+export default Textbox;
