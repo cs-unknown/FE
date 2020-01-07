2b7dbde4845248009d5b4ed46adb51115cb1e306
Priyanka Dadlani <dadlani.priyanka@gmail.com>
2020-01-07 13:50:02 -0800

    added components and sidebar.

--- cs-unknown/src/App.js
+++ cs-unknown/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Medieval Labyrinth
+        <Sidebar />
       </header>
     </div>
   );
