286c3c9c964c3a318e2ef12e051e9102dfe65bca
antilou86 <antilou86@gmail.com>
2020-01-08 13:59:39 -0500

    wired the redux store to Movement.js and Textbox.js, need to style a bit before i can adequately test it out and debug.

--- cs-unknown/src/components/Sidebar/Textbox.js
+++ cs-unknown/src/components/Sidebar/Textbox.js
@@ -1,18 +1,26 @@
 import React, {useState, useEffect} from 'react';
+import {useSelector} from 'react-redux'
+
 
 
 //for each response to a move call, the response.data will have to be passed in here as props.
 
-const Textbox = (props) => {
+const Textbox = () => {
     const [text, setText] = useState(["Welcome to Game Unknown.", "*A commanding voice echoes through the chamber you are standing in and rebounds throughout your skull.*","*a sense of dark dread fills your being, like when you have to use the restroom, but you are nowhere near one.*", "'Good luck figuring out how to win.'", "'MUAAAAHAHAHAHAAAAAAA'",])
+    const title = useSelector(state => state.title)
+    const description = useSelector(state => state.description)
+    const players = useSelector(state => state.players)
+    const error_msg = useSelector(state => state.error_msg)
     
-
     useEffect(()=> {
-        setText([...text] + props.newText)
-    },[props.text]) //double check this dependency array for naming convention.
+        //if there is an error message it add that to the scroll. if not it adds room title and description.
+        error_msg ? setText(`${error_msg}`) : (setText([...text] + `you have entered ${title}` + `${description}`))
+        players ? setText(`players with you: ${players}`) : setText("you are alone in this room") 
+    },[title, error_msg]) //useEffect should only fire if title or error_msg change.
     
     return (
     //css will have to allow for scrolling overflow of text in this box.
+    //i'm going to have to look into getting the bottom part of the scroll to show what is happening.
       <div className='textbox'>
         {text.map((line, i) => {
             <p key={"line"+i}>{line}</p>
