0c200bbf77aa9ce9c5a34b7e53b7143daaa5c9e0
Priyanka Dadlani <dadlani.priyanka@gmail.com>
2020-01-09 07:31:19 -0800

    mediocre layout.

--- cs-unknown/src/components/Sidebar/Sidebar.js
+++ cs-unknown/src/components/Sidebar/Sidebar.js
@@ -11,7 +11,7 @@ const Sidebar = () => {
     <div className="sidebar-container">
       <Textbox />
       <TopSB />
-      <MiddleSB />
+      {/* <MiddleSB /> */}
       <BottomSB />
     </div>
   );
