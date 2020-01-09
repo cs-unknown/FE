2cb969c80d22d5ec1c2428863aca3a0b83233041
brellin <cekakid@gmail.com>
2020-01-08 18:51:14 -0600

    Shitty ass MVP

--- cs-unknown/src/components/MapModal/Coordinate.js
+++ cs-unknown/src/components/MapModal/Coordinate.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Coordinate = props => {
 
-    return <p style={{ background: 'black', color: props.currentRoom ? 'red' : 'white' }}>{props.coo.title}</p>
+    return <p style={{
+        background: 'black',
+        color: props.currentRoom === props.coo.title ? 'red' : 'white',
+        width: '10%',
+        margin: 0,
+        border: '1px solid white',
+        padding: 5
+    }}>{props.coo.title}</p>
 
 }
 
