bd0723df428e7961b6beb18bdb4602bc2393d725
antilou86 <antilou86@gmail.com>
2020-01-09 18:51:33 -0500

    adjusted the textbox size and display so it looks less wonky.

--- cs-unknown/src/components/Sidebar/Textbox.js
+++ cs-unknown/src/components/Sidebar/Textbox.js
@@ -1,33 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux'
+import React, {useState, useEffect} from 'react';
+import {useSelector} from 'react-redux'
 import './Sidebar.css'
-import MapModal from '../MapModal/MapModal'
+
 
 //for each response to a move call, the response.data will have to be passed in here as props.
 
 const Textbox = () => {
-  const [text, setText] = useState(["Welcome to Game Unknown.", "*A commanding voice echoes through the chamber you are standing in and rebounds throughout your skull.*", "*a sense of dark dread fills your being, like when you have to use the restroom, but you are nowhere near one.*", "'Good luck figuring out how to win.'", "'MUAAAAHAHAHAHAAAAAAA'",])
-  const title = useSelector(state => {
-    console.log("useSelector State: ", state)
-    return state.title
-  })
-  const description = useSelector(state => state.description)
-  const players = useSelector(state => state.players)
-  const error_msg = useSelector(state => state.error_msg)
+    const [text, setText] = useState([ "Welcome to Game Unknown.", "*A commanding voice echoes through the chamber you are standing in and rebounds throughout your skull.*","*a sense of dark dread fills your being, like when you have to use the restroom, but you are nowhere near one.*", "'MUAAAAHAHAHAHAAAAAAA'", "'Good luck escaping.'",])
+    const description = useSelector(state => state.description)
+    const players = useSelector(state => state.players)
+    const error_msg = useSelector(state => state.error_msg)
 
-  useEffect(() => {
-    console.log(description)
-    //if there is an error message it add that to the scroll. if not it adds room title and description.
-    error_msg ? setText([`${error_msg}`]) : (setText([`${description}`, (players.length ? `players with you: ${players}` : "you are alone in this room")]))
-    //if players are in the room you've just entered it should show in the text box. if not, print that you're alone
-  }, [description, error_msg]) //useEffect should only fire if description or error_msg change.
-
-  return (
+    useEffect(()=> {
+        //if there is an error message it add thatsz
+        error_msg ? setText([...text,`${error_msg}`]) : (setText([...text, (players.length ? `players with you: ${players}` : "you are all alone"),`${description}`, `..............`]))
+    
+    },[description, error_msg]) //double check this dependency array for naming convention.
+    
+    return (
     //css will have to allow for scrolling overflow of text in this box.
-    //i'm going to have to look into getting the bottom part of the scroll to show what is happening.
-    <div className='textbox'>
-      {text.map((line, i) => <p key={"line" + i}>{line}</p>)}
-    </div>
+      <div className='textbox'>
+        {text.reverse().map((line, i) => <p key={"line" + i}>{line}</p>)}
+      </div>
   )
 }
 
