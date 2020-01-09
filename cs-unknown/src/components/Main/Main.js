9b66fe5ea58e512b17568f6c2cee80a5da9b124c
Priyanka Dadlani <dadlani.priyanka@gmail.com>
2020-01-09 02:08:00 -0800

    started styling.

--- cs-unknown/src/components/Main/Main.js
+++ cs-unknown/src/components/Main/Main.js
@@ -16,15 +16,18 @@ const Main = () => {
     }, [])
 
     return (
-        <div>
-            {/* should house the overlay and animations from jason whatever we end up calling that.*/}
-            <div> background/room image element </div>
+        <div className='main-container'>
+            <div className='main-container-1'>
+                {/* should house the overlay and animations from jason whatever we end up calling that.*/}
+                <div className='sub-container-1'> background/room image element </div>
 
+                {/* rests under the background/room element */}
+                <Movement />
+
+            </div>
+            
             {/* floated to the right */}
             <Sidebar />
-
-            {/* rests under the background/room element */}
-            <Movement />
         </div>
     )
 }
